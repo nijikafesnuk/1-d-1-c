de7256e25424826299a5a88cb27a2df7d2d75615
Agas Pratama <165492024+nijikafesnuk@users.noreply.github.com>
2025-01-06 21:11:21 +0700

    day 2

---
+++ day2/if-expression.js
@@ -0,0 +1,13 @@
+// Membuat  if ekspresi
+
+const nilaiUjian = 90;
+
+if(nilaiUjian > 89) {
+    console.log("Kerja Bagus!");
+} else if(nilaiUjian > 79) {
+    console.log("Tingkatkan!")
+} else if(nilaiUjian > 69) {
+    console.log("Perbaiki Warna Kulitmu!")
+} else {
+    console.log("GET OUT!")
+}
